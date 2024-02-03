7fa6581776dfb70340e1dca8a22e0ccbb006b0a1
Jake Steele <jake.steele@frozenmountain.com>
2024-02-02 16:37:56 -0800

    Added more information to Aliases

--- Namespaces.ts
+++ Namespaces.ts
@@ -16,3 +16,17 @@ validators["moveSpaces"] = new Validator.MoveSpacesValidator();
 validators["moveSpaces"].isValid(10); // True
 validators["moveSpaces"].isValid(101); // False
 validators["moveSpaces"].isValid(-1); // False
+
+// Aliases
+
+namespace CustomValidators // This would be considered bad namespacing. 
+{
+    export namespace PlayerValidators {
+        export class MoveSpeedValidator { isValid: (input: number) => true; }
+    }
+}
+import scopedValidators = CustomValidators.PlayerValidators;
+const moveSpeedValidator = new scopedValidators.MoveSpeedValidator();
+moveSpeedValidator.isValid(10); // True
+moveSpeedValidator.isValid(101); // True
+moveSpeedValidator.isValid(-1); // True
